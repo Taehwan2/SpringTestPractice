f78c821b909ebadb360e18ba8e75f90c85f03c8e
Taehwan2 <taehwansw@gmail.com>
2023-10-14 17:39:06 +0900

    프로젝트 시작

---
+++ src/test/java/com/jyujyu/dayonetest/dayonetest/DayonetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyujyu.dayonetest.dayonetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayonetestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
